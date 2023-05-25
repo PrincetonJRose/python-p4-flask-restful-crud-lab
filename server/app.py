7fdd04a30165594464d09aeeb5d98330de2a629d
Princeton J. Rose <princetonjrose@gmail.com>
2023-05-25 14:22:50 -0400

    got to a good understanding point

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from werkzeug.exceptions import NotFound, UnprocessableEntity
 
 from models import db, Plant
 
@@ -15,6 +16,7 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
 
 class Plants(Resource):
 
@@ -41,12 +43,34 @@ api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
 
+    def find_plant ( self, id ) :
+        plant = Plant.query.filter_by(id=id).first()
+        if plant :
+            return plant
+        else : abort( 404, 'Could not find plant.' )
+
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = self.find_plant( id ).to_dict()
+        return make_response( jsonify( plant ) , 200 )
+    
+    def patch( self, id ) :
+        plant = self.find_plant( id )
+        updates = request.get_json()
+        for key in updates :
+            setattr( plant, key, updates[ key ] )
+        
+        db.session.add( plant )
+        db.session.commit()
+        return make_response( jsonify( plant.to_dict() ), 200 )
+        
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-        
+
+@app.errorhandler( NotFound )
+def handle_not_found ( e ) :
+    return make_response( 'Resource could not be located. Check the url and try again.', 404 )
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
